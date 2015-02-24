1af251a82143621b3e7c9780a7028149c3b3a1b8
kevinleclair1 <kevinleclair1@gmail.com>
2015-02-24 17:28:57 -0500

    first styles and js being added to first input template

---
+++ min/main-min.js
@@ -0,0 +1,1 @@
+var $input=$("input");$(function(){$input.on("focus",function(){console.log("focused"),$(".placeholder").animate({top:"-30%"}),$(".input").animate({paddingTop:"20px",paddingBottom:"20px"})}),$input.on("focusout",function(){console.log("unfocused"),$(".placeholder").animate({top:"50%"}),$(".input").animate({padding:"5px"})})});
