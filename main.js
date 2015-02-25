9e2e7f6171f5e3459f41b67690ebc920318a84fa
kevinleclair1 <kevinleclair1@gmail.com>
2015-02-25 16:44:45 -0500

    finished hard coding three styles, beginning to build plugin infrastucture with jquery.  need to set up options and make more dynamic based on user choices

--- main.js
+++ main.js
@@ -1,56 +1,88 @@
 var $style2 = $('.input2');
+var $style3 = $('.input3');
 var $input1 = $('.style1');
 var $input2 = $('.style2');
+var $input3 = $('.style3');
 var $place1 = $('.placeholder1');
 var $place2 = $('.placeholder2');
+var $place3 = $('.placeholder3');
 var styleSpeed = 200;
 $(function() {
 	//style 1
 	$input1.on('focus', function(){
-		console.log('focused');
-		$('.placeholder1').animate({
-			top: '-30%',
-		});
-		$('.input').animate({
-			paddingTop: '2em',
-			paddingBottom: '2em',
-		});
+		if ( $input1.val() === "") {
+			console.log('focused');
+			$('.placeholder1').animate({
+				top: '-30%',
+			});
+			$('.input').animate({
+				paddingTop: '2em',
+				paddingBottom: '2em',
+			});
+		};
 	});
 	$place1.on('click', function(){
 		$input1.trigger('focus');
 	});
-
 	$input1.on('focusout', function(){
-		console.log('unfocused');
-		$('.placeholder1').animate({
-			top: '50%',
-		});
-		$('.input').animate({
-			padding: '1em',
-		})
+		if ( $input1.val() === "") {
+			console.log('unfocused');
+			$('.placeholder1').animate({
+				top: '50%',
+			});
+			$('.input').animate({
+				padding: '1em',
+			})
+		};
 	});
 	//style 2
 	$style2.on('click', function(){
-		console.log('clicked');
-		$input2.animate({
-			fontSize: '30px',
-			padding: '15px',
-			paddingLeft: '5px',
-		}, styleSpeed);
-		$place2.animate({
-			fontSize: '1.3em'
-		}, styleSpeed);
-		$input2.trigger('focus');
+		if ( $input2.val() === "") {
+			console.log('clicked');
+			$input2.animate({
+				fontSize: '30px',
+				padding: '15px',
+				paddingLeft: '5px',
+			});
+			$place2.animate({
+				fontSize: '1.3em'
+			});
+			$input2.trigger('focus');
+		};
 	});
 	$input2.on('focusout', function(){
-		$input2.animate({
-			fontSize: '0px',
-			padding: '0'
-		}, styleSpeed);
-		$place2.animate({
-			fontSize: '30px'
-		}, styleSpeed)
+		if ( $input2.val() === "") {
+			$input2.animate({
+				fontSize: '0px',
+				padding: '0'
+			});
+			$place2.animate({
+				fontSize: '30px'
+			}, styleSpeed)
+		};
 	});
 	//style 3
-	
+	$place3.on('click', function(){
+		if ( $input3.val() === "") {
+			$input3.animate({
+				left: '0',
+			})
+			$place3.animate({
+				width: '30%',
+				left: '70%',
+			})
+			$input3.trigger('focus');
+		};
+	});
+	$input3.on('focusout', function(){
+		if ( $input3.val() === "") {
+			$input3.animate({
+				left: '-70%',
+			})
+			$place3.animate({
+				width: '100%',
+				left: '0',
+			})
+		};
+	});
 });
